7dc85b273567e2b5beeb3893c3c39520429a30a1
Diogo L Andrade <127452332+soothsayerdev@users.noreply.github.com>
2024-10-29 20:52:04 -0300

    Update Docker

--- exampleDocker/exemplo/src/main/java/com/exemplo/models/Account.java
+++ exampleDocker/exemplo/src/main/java/com/exemplo/models/Account.java
@@ -24,50 +24,50 @@ public class Account {
 
     private Double balance;
 
-    private List<Transaction> transactions;
-
-    public Account(){}
-
-    public Account(String owner){
-        this(owner, 0.0);
-    }
-
-    public Account(String owner, Double balance){
-        this.owner = owner;
-        this.balance = balance;
-    }
-
-    public void makeDeposit(Double amount, LocalDateTime date, String notes) {
-        if(amount <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.transactions.add(new Transaction(amount, date, notes));
-        this.balance += amount;
-    }
-    public void makeWithdrawal(Double amount, LocalDateTime date, String notes, Double balance) {
-        if(amount <= 0){
-            throw new IllegalArgumentException();
-        }
-
-        else if(balance - amount < 0){
-            throw new IllegalStateException();
-        }
-
-        this.transactions.add(new Transaction(amount, date, notes));
-        this.balance -= amount;
-
-    }
-
-    public String getAccountHistory(){
-        StringBuilder report = new StringBuilder();
-        report.append("Date\tAmount\tBalance\tNotes");
-
-        for(Transaction transaction : this.transactions){
-            balance += transaction.getAmount();
-            report.append(transaction.getDate() + " " + transaction.getAmount() + " " + 0.0 +
-                    " " + transaction.getNotes());
-        }
-        return report.toString();
-    }
+//    private List<Transaction> transactions;
+//
+//    public Account(){}
+//
+//    public Account(String owner){
+//        this(owner, 0.0);
+//    }
+//
+//    public Account(String owner, Double balance){
+//        this.owner = owner;
+//        this.balance = balance;
+//    }
+//
+//    public void makeDeposit(Double amount, LocalDateTime date, String notes) {
+//        if(amount <= 0) {
+//            throw new IllegalArgumentException();
+//        }
+//        this.transactions.add(new Transaction(amount, date, notes));
+//        this.balance += amount;
+//    }
+//    public void makeWithdrawal(Double amount, LocalDateTime date, String notes, Double balance) {
+//        if(amount <= 0){
+//            throw new IllegalArgumentException();
+//        }
+//
+//        else if(balance - amount < 0){
+//            throw new IllegalStateException();
+//        }
+//
+//        this.transactions.add(new Transaction(amount, date, notes));
+//        this.balance -= amount;
+//
+//    }
+//
+//    public String getAccountHistory(){
+//        StringBuilder report = new StringBuilder();
+//        report.append("Date\tAmount\tBalance\tNotes");
+//
+//        for(Transaction transaction : this.transactions){
+//            balance += transaction.getAmount();
+//            report.append(transaction.getDate() + " " + transaction.getAmount() + " " + 0.0 +
+//                    " " + transaction.getNotes());
+//        }
+//        return report.toString();
+//    }
 
 }
--- exampleDocker/exemplo/src/main/java/com/exemplo/models/SavingsAccount.java
+++ exampleDocker/exemplo/src/main/java/com/exemplo/models/SavingsAccount.java
@@ -1,27 +1,27 @@
-package com.exemplo.models;
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Table
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-public class SavingsAccount extends Account {
-
-    private Double annualRate = 0.001;
-
-    public SavingsAccount(String owner, Double balance, Double annualRate) {
-        super(owner, balance);
-        if (annualRate > 0.0) {
-            this.annualRate = annualRate;
-        }
-    }
-}
+//package com.exemplo.models;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Entity
+//@Table
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+//public class SavingsAccount extends Account {
+//
+//    private Double annualRate = 0.001;
+//
+//    public SavingsAccount(String owner, Double balance, Double annualRate) {
+//        super(owner, balance);
+//        if (annualRate > 0.0) {
+//            this.annualRate = annualRate;
+//        }
+//    }
+//}
